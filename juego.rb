42ded8d56bb8ca36d0f103f63bbd42d256f56aad
proyectos-js <github.proyectos.js@gmail.com>
2020-06-11 23:34:39 -0400

    Desafio Juego

---
+++ juego.rb
@@ -0,0 +1,50 @@
+jugador = ARGV[0].downcase
+computador = rand(0..2)
+
+# 0 es piedra, 1 es papel, 2 tijera
+
+case(jugador)
+when 'piedra'
+    if computador==0
+        puts "computador juega piedra \nempate"
+    elsif computador==1
+        puts 'computador juega papel'
+        puts 'computador gana'
+    else
+        puts 'computador juega tijera'
+        puts 'jugador gana'
+    end
+
+when 'papel'
+    if computador==0
+        puts 'computador juega piedra'
+        puts 'jugador gana'
+    elsif computador==1
+        puts 'computador juega papel'
+        puts 'empate'
+    else
+        puts 'computador juega tijera'
+        puts 'computador gana'
+    end
+
+when 'tijera'
+    if computador==0
+        puts 'computador juega piedra'
+        puts 'computador gana'
+    elsif computador==1
+        puts 'computador juega papel'
+        puts 'jugador gana'
+    else
+        puts 'computador juega tijera'
+        puts 'empate'
+    end
+
+else
+    puts 'Argumento invalido: Debe ser piedra, papel o tijera'
+end
+
+
+
+
+
+
